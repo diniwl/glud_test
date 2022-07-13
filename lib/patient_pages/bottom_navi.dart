dbef140ee2fe634b543332cd0aee2696a212843e
fltterchive <diniiiwl@gmail.com>
2022-07-13 22:31:47 +0700

    Add Food Log Navi

--- lib/patient_pages/bottom_navi.dart
+++ lib/patient_pages/bottom_navi.dart
@@ -1,66 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:glud_test/patient_pages/consult_page.dart';
+import 'package:glud_test/patient_pages/foodlog_page.dart';
 import 'package:glud_test/patient_pages/home_page.dart';
 import 'package:glud_test/patient_pages/report_page.dart';
 import 'home_page.dart';
 import 'consult_page.dart';
 import 'report_page.dart';
+import 'foodlog_page.dart';
 
-
-class BottomNavi extends StatefulWidget{
-
+class BottomNavi extends StatefulWidget {
   @override
   State<BottomNavi> createState() => _BottomNaviState();
 }
-
 
 class _BottomNaviState extends State<BottomNavi> {
   int _currentIndex = 0;
   //ypu can change the list later with the fixed pages
   final List<Widget> body = [
-    HomePage(payload: '',),
+    HomePage(
+      payload: '',
+    ),
     BarChartSample1(),
+    FoodLog(),
     ConsultPage(),
-
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
+    return Scaffold(
       backgroundColor: Color(0xff273248),
-        body: body[_currentIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.white,
-          iconSize: 30,
-          items: [
-            //urutannya harus sama di urutan list final tabs
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              backgroundColor: Colors.grey,
-              label: 'Home',
-            ),
-            
-            BottomNavigationBarItem(
-              icon: Icon(Icons.assessment),
-              backgroundColor: Colors.grey,
-              label: 'Report',
-            ),
-
-            BottomNavigationBarItem(
-              icon: Icon(Icons.chat),
-              backgroundColor: Colors.grey,
-              label: 'Consult',
-              ),
-          ],
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
+      body: body[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        iconSize: 30,
+        items: [
+          //urutannya harus sama di urutan list final tabs
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            backgroundColor: Colors.grey,
+            label: 'Home',
           ),
-  );
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assessment),
+            backgroundColor: Colors.grey,
+            label: 'Report',
+          ),
+          
+          BottomNavigationBarItem(
+              icon: Icon(Icons.food_bank_rounded),
+              backgroundColor: Colors.grey,
+              label: 'Food Log',),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            backgroundColor: Colors.grey,
+            label: 'Consult',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
-
---
+++ lib/patient_pages/foodlog_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FoodLog extends StatefulWidget {
+  const FoodLog({Key? key}) : super(key: key);
+
+  @override
+  State<FoodLog> createState() => _FoodLogState();
+}
+
+class _FoodLogState extends State<FoodLog> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+    
+  }
+}
